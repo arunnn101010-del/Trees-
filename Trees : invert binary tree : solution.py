a3a3939ad6eecf585f60c23aa8dce83dd9c5ca2d
Arun <arunnn101010@gmail.com>
2026-06-22 22:31:35 +0530

    Update Trees : invert binary tree : solution.py

--- Trees : invert binary tree : solution.py
+++ Trees : invert binary tree : solution.py
@@ -1,1 +1,21 @@
+# Promblem - invert binary tree 
+# Approach - recursion 
+# Time and space complexity - 0(n) & 0(h) 
+# Leetcode and diffculty level - 226 & easy 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL && q == NULL) {
+            return true;
+        }
+        if(p == NULL || q == NULL) {
+            return false;
+        }
+        if(p->val != q->val) {
+            return false;
+        }
 
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
