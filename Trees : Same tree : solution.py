8fa9ac1ca47fb6b386201e8d8039aad071770332
Arun <arunnn101010@gmail.com>
2026-06-22 21:38:32 +0530

    Update Trees : Same tree : solution.py

--- Trees : Same tree : solution.py
+++ Trees : Same tree : solution.py
@@ -1,1 +1,21 @@
+# Promblem - same tree 
+# Approach - recursion
+# Time and space complexity - 0(n) & 0(h) 
+# Leetcode and diffculty level - 100 & easy 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL && q == NULL) {
+            return true;
+        }
+        if(p == NULL || q == NULL) {
+            return false;
+        }
+        if(p->val != q->val) {
+            return false;
+        }
 
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
