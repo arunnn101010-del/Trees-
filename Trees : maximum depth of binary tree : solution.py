65a577d9a781c45b015af1d681ff567823273a89
Arun <arunnn101010@gmail.com>
2026-06-22 21:20:20 +0530

    Update Trees : maximum depth of binary tree : solution.py

--- Trees : maximum depth of binary tree : solution.py
+++ Trees : maximum depth of binary tree : solution.py
@@ -1,1 +1,17 @@
+# Promblem - maximum depth of binary tree 
+# Approach - recursion 
+# Time and space complexity - 0(n) & 0(h) 
+# Leetcode and diffculty level - 104 & easy 
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
 
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+
+        return 1 + max(left, right);
+    }
+};
