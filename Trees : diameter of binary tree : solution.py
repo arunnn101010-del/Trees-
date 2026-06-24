7f1c2f8a7bff9f3d2dbe255b6a62f8fbaf1e2a5a
Arun <arunnn101010@gmail.com>
2026-06-24 23:09:32 +0530

    Update Trees : diameter of binary tree : solution.py

--- Trees : diameter of binary tree : solution.py
+++ Trees : diameter of binary tree : solution.py
@@ -1,4 +1,28 @@
 # Promblem - diameter of a binary tree 
-# Approach - 
-# Time and space complexity - 
-# Leetcode and diffculty level - 
+# Time and space complexity - 0(n) & o(h)
+# Leetcode and diffculty level - 543 & easy 
+class Solution {
+public: 
+    int ans = 0;
+
+    int height(TreeNode* root) {
+
+        if(root == NULL) 
+            return 0;
+            
+        int left = height(root->left);
+        int right = height(root->right);
+
+        ans = max(ans, left + right);
+
+        return 1 + max(left, right);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+
+        height(root);
+
+        return ans;
+    }
+    
+};
