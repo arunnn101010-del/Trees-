7ee60ed469d0551a59eecf8c6d469a07a16add10
Arun <arunnn101010@gmail.com>
2026-07-01 21:19:34 +0530

    Update Tree : validate binary search tree : solution.py

--- Tree : validate binary search tree : solution.py
+++ Tree : validate binary search tree : solution.py
@@ -1,1 +1,24 @@
+# Problem - validate binary search tree 
+# Approach - dfs 
+# Time and space complexity - 0(n) & 0(h) 
+# Leetcode and diffculty level - 98 & medium 
+class Solution {
+public:
 
+    bool check(TreeNode* root, long minVal, long maxVal) {
+
+        if(root == NULL) {
+            return true;
+        }
+
+        if(root->val <= minVal || root->val >= maxVal) {
+            return false;
+        }
+
+        return check(root->left, minVal, root->val) && 
+            check(root->right, root->val, maxVal);
+    }
+    bool isValidBST(TreeNode* root) {
+        return check(root, LONG_MIN, LONG_MAX);
+    }
+};
